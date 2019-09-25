2c24d1c0e95a74b88ab182669d2e9c37d40a3729
yang zhang <tianna1121@gmail.com>
2019-09-25 16:28:08 +0800

    add blogs part

--- js/main.js
+++ js/main.js
@@ -10,9 +10,13 @@ $(document).ready(function() {
   // owl-carousel for blog
   $(".owl-carousel").owlCarousel({
     loop: true,
-    autoplay: true,
+    autoplay: false,
     autoplayTimeout: 3000,
     dots: false,
-    nav: true
+    nav: true,
+    navText: [
+      $(".owl-navigation .owl-nav-prev"),
+      $(".owl-navigation .owl-nav-next")
+    ]
   });
 });
