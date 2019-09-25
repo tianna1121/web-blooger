665eef98434954e5a0ff64b77951401804a52791
yang zhang <tianna1121@gmail.com>
2019-09-25 23:03:05 +0800

    make blogs section responsive

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+const responsive = {
+  320: {
+    items: 1
+  },
+  560: {
+    items: 2
+  },
+  960: {
+    items: 3
+  }
+};
+
 $(document).ready(function() {
   $nav = $(".nav");
   $toggleCollapse = $(".toggle-collapse");
@@ -17,7 +29,8 @@ $(document).ready(function() {
     navText: [
       $(".owl-navigation .owl-nav-prev"),
       $(".owl-navigation .owl-nav-next")
-    ]
+    ],
+    responsive: responsive
   });
 
   // click to scroll up
